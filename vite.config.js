52d53c4ae7403fb7818e4537885cce03a76c50e0
Gaurav Senger <gaurav.senger@payu.in>
2025-11-18 11:02:04 +0530

    dev

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    host: '0.0.0.0',
     open: true
+  },
+  preview: {
+    host: '0.0.0.0',
+    strictPort: false
   }
 })
 
